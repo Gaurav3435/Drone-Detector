0c8e51caf32ca665b589bb5e569deb3e2f77bcc6
Hrugved Kolhe <geeky.hrugved143@gmail.com>
2022-01-24 04:00:10 +0530

    predictiction

---
+++ App/predict/index.js
@@ -0,0 +1,20 @@
+let net = null;
+
+
+ function showFiles() {
+    // An empty img element
+    let demoImage = document.getElementById('idImage');
+    // read the file from the user
+    let file = document.querySelector('input[type=file]').files[0];
+    const reader = new FileReader();
+    reader.onload = function (event) {
+        demoImage.src = reader.result;
+    }
+    reader.readAsDataURL(file);
+    app();
+}  
+
+$("#files").change(function() {
+    filename = this.files[0].name;
+    console.log(filename);
+  });
